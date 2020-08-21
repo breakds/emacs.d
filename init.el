ef481a960dd773f86ef9f509dd076e98f0a968ad
Break Yang <breakds@gmail.com>
2020-08-20 23:29:12 -0700

    Start from beginning with straight.el

--- init.el
+++ init.el
@@ -26,102 +26,48 @@
 ;; Enable Common Lisp
 (require 'cl)
 
-;; Set up the load path for ELPA/MELPA packages.
-;; - ELPA is the Emacs Lisp Package Archive
-;; - MELPA is yet another repository
-;; Note that Emacs users in China mainland can use '("popkit" . "http://elpa.popkit.org/packages/")
-(require 'package)
-(package-initialize nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; Bootstrap straight.el
 
-;; There seems to be a bug for Emacs 26.1 and 26.2, so that it fails
-;; to download packages from "gnu" because of some tls certificate
-;; issue. This fixes that by using 163.com mirror if needed.
-;;
-;; (setq package-archives '(("gnu" . "http://mirrors.163.com/elpa/gnu/")
-;;                          ("melpa" . "https://melpa.org/packages/")
-;;                          ("org" . "http://orgmode.org/elpa/")))
+(defvar bootstrap-version)
+(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el"
+					user-emacs-directory))
+      (bootstrap-version 5)
+      (straight-url "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously straight-url 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; A dedicate place for emacs `customize` interface
-(setq custom-file "~/.emacs.d/customize.el")
-(load custom-file)
+;; Solarized Theme
+(straight-use-package 'color-theme-modern)
+(straight-use-package 'color-theme-sanityinc-solarized)
+(load-theme 'sanityinc-solarized-light t)
 
-;; Packages for bootstrapping the configuration
-(defmacro bootstrap-install (&rest packages)
-  "This macro ensures that the input packages are all installed."
-  `(let ((need-refresh nil))
-     (loop for x in (list ,@packages)
-	   when (not (package-installed-p x))
-	   do (setf need-refresh t))
-     (when need-refresh
-       (package-refresh-contents)
-       ,@(loop for x in packages
-	       collect `(when (not (package-installed-p ,x))
-			  (package-install ,x))))))
+;; Diminish
+;; --------
+;; Hide certain major/minor mode in the mode line
+;; Example Usage, see https://github.com/emacsmirror/diminish
+(straight-use-package 'diminish)
 
-;; Install bootstrapping packages.
-;; 1. use-package for loading packages and local directories.
-(bootstrap-install 'use-package)
-
-;; Set up use-package.
-(require 'use-package)
-
-;; Use load-dir to load the configuration directory which is set to
-;; "~/.emacs.d/personal"
-;; Ensure that the general configuration is loaded first.
-(load "~/.emacs.d/general.el")
-(use-package load-dir :ensure t)
-(add-to-list 'load-dirs "~/.emacs.d/personal")
-(load-dirs)
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(TeX-view-program-list (quote (("Atril" "atril %o"))))
-;;  '(TeX-view-program-selection
-;;    (quote
-;;     (((output-dvi style-pstricks)
-;;       "dvips and gv")
-;;      (output-dvi "xdvi")
-;;      (output-pdf "Atril")
-;;      (output-html "xdg-open"))))
-;;  '(ledger-reports
-;;    (quote
-;;     (("2018" "ledger ")
-;;      ("bal" "%(binary) -f %(ledger-file) bal")
-;;      ("reg" "%(binary) -f %(ledger-file) reg")
-;;      ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
-;;      ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
-;;  '(markdown-command "grip --export -")
-;;  '(org-agenda-files
-;;    (quote
-;;     ("~/org/cron.org" "~/org/reading.org" "~/org/unsorted.org" "~/org/projects.org")))
-;;  '(package-selected-packages
-;;    (quote
-;;     (ledger-mode nix-mode dockerfile-mode json-mode tide web-mode-edit-element clang-format cmake-ide rtags flycheck company protobuf-mode yaml-mode web-mode use-package toc-org solarized-theme smart-mode-line org-doing org-dashboard markdown-mode+ load-dir forecast fcitx exec-path-from-shell elm-mode ein cargo))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(custom-safe-themes
-;;    (quote
-;;     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
-;;  '(package-selected-packages
-;;    (quote
-;;     (tabbar session pod-mode org muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode use-package load-dir))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
+;; el-patch
+;; --------
+;; Provides a way to customize the behavior of Emacs Lisp functions
+;; that do not provide enough variables and hooks to let you
+;; customize.
+(straight-use-package 'el-patch)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
