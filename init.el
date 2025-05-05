1ef283fe9af6bc7e83a071346cce09713ab6aef5
Break Yang <breakds@gmail.com>
2025-05-05 13:55:45 -0700

    Update theme

--- init.el
+++ init.el
@@ -93,12 +93,26 @@
 					 user-emacs-directory)))
 
 (bds-load-config-module "modules" "appearance.org")
-(bds-load-config-module "modules" "ivy.org")
-(bds-load-config-module "modules" "basic.org")
-(bds-load-config-module "modules" "org-tweaks.org")
-(bds-load-config-module "modules" "development.org")
+;; (bds-load-config-module "modules" "ivy.org")
+;; (bds-load-config-module "modules" "basic.org")
+;; (bds-load-config-module "modules" "org-tweaks.org")
+;; (bds-load-config-module "modules" "development.org")
 (garbage-collect)
 
 ;; Now that startup is completed. Descreasing the GC threshold (i.e.
 ;; more frequently GC).
 (setq gc-cons-threshold (* 20 1000 1000))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
